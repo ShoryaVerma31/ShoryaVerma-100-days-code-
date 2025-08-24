6a1a10d671912c1fbd317235f80fbfbab1792b58
Shorya Verma <shoryaverma3105@gmail.com>
2025-08-24 16:24:56 +0530

    Daily Question challenges

---
+++ Q1.c
@@ -0,0 +1,12 @@
+//Q1 (User Inputs, Operations & Output)
+//Write a program to input two numbers and display their sum.
+
+#include<stdio.h>
+int main(){
+    int first, second, sum;
+    printf("Enter your two number");
+    scanf("%d%d", &first ,&second);
+    sum = first + second;
+    printf("The sum of your two number is %d\n", sum);
+    return 0;
+}
