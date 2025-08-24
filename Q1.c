b1741f57b8cc65dad266a0b19ccd60e9ada56676
Shorya Verma <shoryaverma3105@gmail.com>
2025-08-24 17:43:44 +0530

    Q1.c

--- Q1.c
+++ Q1.c
@@ -4,9 +4,10 @@
 #include<stdio.h>
 int main(){
     int first, second, sum;
-    printf("Enter your two number");
+    printf("Enter your two number\n");
     scanf("%d%d", &first ,&second);
     sum = first + second;
     printf("The sum of your two number is %d\n", sum);
     return 0;
+
 }
