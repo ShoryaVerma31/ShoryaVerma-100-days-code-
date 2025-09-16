a25629948f7d925252a9fa4eba335a8e6221d957
ShoryaVerma31 <shoryaverma3105@gmail.com>
2025-09-17 00:12:31 +0530

    Daily question challenge

---
+++ Day 22/Q43.c
@@ -0,0 +1,15 @@
+//Q43: Write a program to check if a number is a strong number(a number whose sum of factorial of the digits of the number is equal to original number).
+
+/*
+Sample Test Cases:
+Input 1:
+145
+Output 1:
+Strong number
+
+Input 2:
+123
+Output 2:
+Not strong number
+
+*/
