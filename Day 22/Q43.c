6024ba9aca1eccf50cdec8fa6c12e8c585e72149
ShoryaVerma31 <shoryaverma3105@gmail.com>
2025-09-17 12:06:56 +0530

    Daily question challenge

--- Day 22/Q43.c
+++ Day 22/Q43.c
@@ -13,3 +13,29 @@ Output 2:
 Not strong number
 
 */
+#include<stdio.h>
+
+int main(){
+    int num,a,b,c,sum=0;
+    
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    
+    a=num;
+    while(a > 0){
+        b = a % 10;
+        c =1;
+        for( int i = 1; i <= b; i++){
+            c = c * i;
+        }
+        sum = sum + c;
+        a= a/10;
+    }
+    if( sum == num){
+        printf("strong number");
+    }
+    else{
+        printf("not a strong number");
+    }
+    return 0;
+}
--- Day 22/Q44.c
+++ Day 22/Q44.c
@@ -3,9 +3,9 @@
 /*
 Sample Test Cases:
 Input 1:
-3
+3 
 Output 1:
-Approximate sum: 3.3
+Approximate sum: 3.3 (may be some typo error, output is 2.6)
 
 Input 2:
 5
@@ -13,3 +13,29 @@ Output 2:
 Approximate sum: 4.4
 
 */
+#include<stdio.h>
+
+int main(){
+    int n,i;
+    double sum=0.0;
+
+    printf("Enter the number of terms : ");
+    scanf("%d",&n);
+
+    
+
+    if(n <= 0){
+       printf("Approximate sum: 0.0\n", sum);
+       return 0;
+    }
+    
+    sum = 1.0;           // first term is 1
+    for(i=2; i<=n; i++)  // stars from 2
+    {
+        sum = sum + (double)(2 * i - 1) / (2 * i);
+    }
+    
+    printf("Approximate sum: %.1f\n", sum);
+
+    return 0;
+}
