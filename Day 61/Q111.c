b645d4defa9888ad214c8ef7513f51189c112e98
Shorya Verma <shoryaverma3105@gmail.com>
2025-11-27 22:36:38 +0530

    Daily question challenge

--- Day 61/Q111.c
+++ Day 61/Q111.c
@@ -19,3 +19,37 @@ Output 3:
 0 0
 
 */
+
+#include <stdio.h>
+
+int main() {
+    int n, k;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+    for (int i = 0; i <= n - k; i++) {
+        int found = 0;
+
+        for (int j = i; j < i + k; j++) {
+            if (arr[j] < 0) {
+                printf("%d ", arr[j]);  
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found) {
+            printf("0 ");
+        }
+    }
+
+    return 0;
+}
